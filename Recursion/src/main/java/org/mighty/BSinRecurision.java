dbcd590230197ebb1a9bd6ab1a467b08275c9b12
Raju <mightyrajukushwaha@gmail.com>
2026-08-31 00:26:43 +0530

    Initial

---
+++ Recursion/src/main/java/org/mighty/BSinRecurision.java
@@ -0,0 +1,28 @@
+package org.mighty;
+
+public class BSinRecurision {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,66,67,87};
+        int target = 87;
+        System.out.println(BinarySearch(arr, target,0,arr.length - 1));
+    }
+//return type
+
+    static int BinarySearch(int[] arr,int target, int s, int e){
+        // target doesnot exist in array
+        if (s > e){
+            return -1;
+        }
+        // find mid value in body and s,e should be in arguments
+        int mid = s + ( e -s) / 2;
+        //
+        if(arr[mid]== target){
+            return mid;
+        }
+
+        if(target < arr[mid]){
+            return  BinarySearch(arr,target,s, mid-1); // left side of array
+        }
+        return BinarySearch(arr,target, mid+1, e);// right side of array
+    }
+}
---
+++ Recursion/src/main/java/org/mighty/FibonacciSeries.java
@@ -0,0 +1,13 @@
+package org.mighty;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        System.out.println(Fibo(6));
+    }
+    static  int Fibo(int n){
+        if(n < 2){
+            return n;
+        }
+        return Fibo(n-1) + Fibo(n-2);
+    }
+}
